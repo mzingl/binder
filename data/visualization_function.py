0c0dcb983fad7beced14dcaae8caa07fdd8f9697
Manuel Zingl <manuel.zingl@gmx.at>
2020-02-07 21:42:54 -0500

    Major update towards final version

--- data/visualization_function.py
+++ data/visualization_function.py
@@ -1,127 +1,141 @@
+import warnings; warnings.simplefilter('ignore')
 import numpy as np
-from TB_functions import *
 import copy
 from triqs_tprf.wannier90 import *
+from pytriqs.lattice.tight_binding import energies_on_bz_path
 from matplotlib import cm
 from triqs_tprf.tight_binding import *
-from pytriqs.lattice.tight_binding import energies_on_bz_path, energy_matrix_on_bz_path
-from matplotlib import pylab as plt 
+from matplotlib import pylab as plt
 import skimage.measure
-import matplotlib.patheffects as pe
+import ipywidgets as widgets
+from TB_functions import *
 import matplotlib
 
 # Define K-Points and PATH
 G = np.array([ 0.00, 0.00, 0.00])
 X = np.array([+0.50, 0.00, 0.00])
 Y = np.array([+0.00,+0.50, 0.00])
-
 M = np.array([+0.50,-0.50, 0.00])
 Z = np.array([+0.00,+0.00,+0.50])
 R = np.array([+0.50, 0.00,+0.50])
 A = np.array([+0.50,+0.50,+0.50])
-
 paths = [(G, X), (X, M), (M, G), (G, Z), (Z, R), (R, A), (A, Z)]
 
-TBL = load_data()
+# Load Wannier model
+TBL = get_TBL('/home/manuel/TESTS/binder/data/LNO_ap3_863_W90/')
 
-def func(model = TBL['LNO unstrained'], crystal_field = 0.0, hopping_x = 1.0, hopping_z=1.0, kz_plot1 = 0.0,
-         kz_plot2 = 1.0, Nk_path = 20, Nk_FS = 10):
-    
-    lw = 5
-    hopping = copy.deepcopy(model._hop)
-    hopping[(0,0,0)] += np.array([[-crystal_field/2.0,0],[0,crystal_field/2.0]])
-    hopping[(1,0,0)][0,0] *= hopping_x
-    hopping[(0,1,0)][0,0] *= hopping_x
-    hopping[(-1,0,0)][0,0] *= hopping_x
-    hopping[(0,-1,0)][0,0] *= hopping_x
-    hopping[(0,0,1)][0,0] *= hopping_z
-    hopping[(0,0,-1)][0,0] *= hopping_z
-    
-    hopping[(1,0,0)][1,1] *= hopping_x
-    hopping[(0,1,0)][1,1] *= hopping_x
-    hopping[(-1,0,0)][1,1] *= hopping_x
-    hopping[(0,-1,0)][1,1] *= hopping_x
-    hopping[(0,0,1)][1,1] *= hopping_z
-    hopping[(0,0,-1)][1,1] *= hopping_z
-   
-    hopping[(1,0,0)][0,1] *= hopping_x
-    hopping[(0,1,0)][0,1] *= hopping_x
-    hopping[(-1,0,0)][0,1] *= hopping_x
-    hopping[(0,-1,0)][0,1] *= hopping_x
-    hopping[(0,0,1)][0,1] *= hopping_z
-    hopping[(0,0,-1)][0,1] *= hopping_z
-    
-    hopping[(1,0,0)][1,0] *= hopping_x
-    hopping[(0,1,0)][1,0] *= hopping_x
-    hopping[(-1,0,0)][1,0] *= hopping_x
-    hopping[(0,-1,0)][1,0] *= hopping_x
-    hopping[(0,0,1)][1,0] *= hopping_z
-    hopping[(0,0,-1)][1,0] *= hopping_z
-    
-    tbl = TBLattice(units = model.Units, hopping = hopping, orbital_positions = model.OrbitalPositions,
-                    orbital_names = model.OrbitalNames)
-    
-    beta = 5.0
-    kx = np.linspace(0.0,0.5,Nk_FS)
-    KX,KY,KZ = np.meshgrid(kx,kx,kx)
-    k_points = np.vstack([KX.ravel(), KY.ravel(),KZ.ravel()]).T.reshape(-1,3)
-    sigma = np.zeros(3)
+# Plot function
+def func(d_cf_z2 = 0.0, d_cf_x2y2 = 0.0, hopping_x = 1.0, hopping_z=1.0, 
+         kz = 0.0, Nk_path = 20, Nk_FS = 10):
 
-    Vol = tbl.Units[0][0]*tbl.Units[1][1]*tbl.Units[2][2]
-    E, V = get_E_and_v_for_kgrid(tbl, k_points)
-    for ik in range(len(E)):
-        sigma[0] += V[ik,0]**2.0 *fermi_dis(E[ik],beta=beta)
-        sigma[1] += V[ik,1]**2.0 *fermi_dis(E[ik],beta=beta)
-        sigma[2] += V[ik,2]**2.0 *fermi_dis(E[ik],beta=beta)
-
-    sigma = sigma/(len(E)*Vol)*1e3
-
-    #print calc_N(tbl,nk=NN)
-
-    k, K, E_mat = energy_matrix_on_bz_paths(paths, tbl, Nk_path)
-    E = np.zeros((2,np.shape(E_mat)[2]),dtype=complex)
-    v = np.zeros((2,2,np.shape(E_mat)[2]),dtype=complex)
-    for ik in range(np.shape(E_mat)[2]):
-        E[:,ik], v[:,:,ik] = np.linalg.eig(E_mat[:,:,ik])
+    lw = 10
+    sign = [1,-1]
     
+    # Modify hopping
+    hopping = copy.deepcopy(TBL._hop)
+    for h in hopping:
+        if not any(h):
+            hopping[(0,0,0)] += np.array([[d_cf_z2,0],[0,d_cf_x2y2]])
+       # elif not any(h[0:2]):
+       #hopping[h] *= hopping_z
+        elif h[2] == 0:
+            hopping[h] *= hopping_x
+        else:
+            hopping[h] *= hopping_z
+
+    tbl = TBLattice(units = TBL.Units, hopping = hopping, orbital_positions = TBL.OrbitalPositions,
+                    orbital_names = TBL.OrbitalNames)
+
     fig = plt.figure()
-    plt.subplot(1,3,1)
-
+    plt.subplot(1,2,1)
+    plt.subplots_adjust(wspace=0.4)
+    
+    # Band structure plot
+    k, K, E = energies_on_bz_paths(paths, tbl, Nk_path)
     for bidx in xrange(E.shape[0]):
-        color = v[bidx,1]**2
-        plt.scatter(k, np.real(E[bidx]), c=cm.seismic(np.real(color)), edgecolor='none',linewidth=2)
-        plt.plot([0,300],[0,0],'k-',label=None)
-
+        plt.plot(k, np.real(E[bidx,:]),'ko',ms=4)
+    plt.plot([0,300],[0,0],'k-',label=None)
     plt.grid()
-    plt.xticks(K,[r'$\Gamma$',r'$X$',r'$M$',r'$\Gamma$',r'$Z$',r'$R$',r'$A$',r'$Z$'])
+    plt.xticks(K,[r'$\Gamma$',r'X',r'M',r'$\Gamma$',r'Z',r'R',r'A',r'Z'])
     plt.xlim([K.min(), K.max()])
-    plt.ylabel(r"$\epsilon_{\mathbf{k}}$")
+    plt.ylabel(r"$\epsilon_{\mathbf{k}}$ (eV)")
     plt.ylim(-2,4)
+    
+    # FS plots
+    
+    plt.subplot(1,2,2)
 
-    kz_values = [kz_plot1,kz_plot2]
-    for ikz, kz in enumerate(kz_values):
-        plt.subplot(1,1+len(kz_values),ikz+2)
-        
-        FS_kx_ky, char = get_kx_ky_FS(X,Y,Z,tbl,N_kxy=Nk_FS,kz=kz)
+    FS_kx_ky, char = get_kx_ky_FS(X,Y,Z,tbl,k_trans_back=np.eye(3),N_kxy=Nk_FS,kz=2.0*kz)
 
-        for i in range(len(FS_kx_ky)):
-                for n in range(len(FS_kx_ky[i][:,0])):
-                    plt.plot(FS_kx_ky[i][n:n+2,0],FS_kx_ky[i][n:n+2,1], '-', lw=lw, solid_capstyle='round', color = char[i][n])
-                    plt.plot(-FS_kx_ky[i][n:n+2,0],FS_kx_ky[i][n:n+2,1], '-', lw=lw, solid_capstyle='round', color = char[i][n])
-                    plt.plot(-FS_kx_ky[i][n:n+2,0],-FS_kx_ky[i][n:n+2,1], '-', lw=lw, solid_capstyle='round', color = char[i][n])
-                    plt.plot(FS_kx_ky[i][n:n+2,0],-FS_kx_ky[i][n:n+2,1], '-', lw=lw, solid_capstyle='round', color = char[i][n])
-                i += 1
-        plt.xlim(-0.5,0.5)
-        plt.ylim(-0.5,0.5)
-        plt.title(r'$k_z$ = '+str(round(kz/2.0,3)))
-        plt.xlabel(r'$k_x$')
-        plt.ylabel(r'$k_y$')
-        
-    im = plt.scatter([0,0], [0,0], s = 1, c = [0,1], cmap = cm.seismic)
-    cbaxes = fig.add_axes([0.42, 0.18, 0.01, 0.2]) 
+    for i in range(len(FS_kx_ky)):
+            for n in range(len(FS_kx_ky[i][:,0])):
+                dummy, v = get_E_and_v_at_k(tbl,FS_kx_ky[i][n,:])
+                color = cm.plasma(np.real((v['x']**2+v['y']**2)/2.0))
+                for s1 in sign:
+                    for s2 in sign:
+                        plt.plot(s1*FS_kx_ky[i][n:n+2,0],s2*FS_kx_ky[i][n:n+2,1], '-', 
+                                 lw=lw, color=color, solid_capstyle='round')
+            i += 1
+
+   
+    plt.title(r'$k_z$ = '+str(round(kz,3)))
+    plt.xlabel(r'$k_x$')
+    plt.ylabel(r'$k_y$')
+    plt.xticks([-0.5,0.0,0.5])
+    plt.yticks([-0.5,0.0,0.5])
+    plt.axis('equal') 
+    plt.xlim(-0.55,0.55)
+    plt.ylim(-0.55,0.55)
+    
+    im = plt.scatter([0,0], [0,0], s = 1, c = [0,1], cmap = cm.plasma)
+    cbaxes = fig.add_axes([0.92, 0.30, 0.01, 0.4])
     cb = plt.colorbar(im, cax = cbaxes,ticks=[0,1])
-    cb.ax.set_yticklabels(['z2', 'x2y2'])  # vertically oriented colorbar
-    plt.tight_layout()
+    cb.ax.set_yticklabels(['min','max'])  # vertically oriented colorbar
+    cb.ax.set_title(r' $v^2$')
     plt.show()
-    print 'sigma_x ~', np.round(sigma[0],3), ';  sigma_z ~', np.round(sigma[2],3), 
 
+# widgets
+style = {'description_width': '100px'}
+d_cf_z2_slider = widgets.FloatSlider(min=-1.0, max=1.0, step=0.01, value=0.0, 
+                                     description=r'$\Delta \epsilon_{z^2}$', 
+                                     continuous_update=False, style=style)
+d_cf_x2y2_slider = widgets.FloatSlider(min=-1.0, max=1.0, step=0.01, value=0.0, 
+                                       description=r'$\Delta \epsilon_{x^2-y^2}$', 
+                                       continuous_update=False, style=style)
+s_h_ab_slider = widgets.FloatSlider(min=0.0, max=2.0, step=0.01, value=1.0, 
+                                    description=r'$t_{ab}$ scaling factor', 
+                                    continuous_update=False, style=style)
+s_h_c_slider = widgets.FloatSlider(min=0.0, max=2.0, step=0.01, value=1.0, 
+                                   description=r'$t_{c}$ scaling factor', 
+                                   continuous_update=False, style=style)
+kz_slider = widgets.FloatSlider(min=0.0, max=1.0, step=0.01, value=0.0, 
+                                description=r'$k_z$', 
+                                continuous_update=False, style=style)
+Nk_path_slider = widgets.IntSlider(min=5, max=50, step=5, value=10, 
+                                   description=r'$N_k$', 
+                                   continuous_update=False, style=style)
+Nk_FS_slider = widgets.IntSlider(min=5, max=50, step=5, value=10, 
+                                 description=r'$N_{FS}$', 
+                                 continuous_update=False, style=style)
+reset_button = widgets.Button(description = "Reset")
+
+def reset_values(b):
+    ui.children[1].children[2].value = 10
+    ui.children[1].children[1].value = 10
+    ui.children[0].children[0].value = 0.0
+    ui.children[0].children[1].value = 0.0
+    ui.children[0].children[2].value = 1.0
+    ui.children[0].children[3].value = 1.0
+    ui.children[1].children[0].value = 0.0
+
+reset_button.on_click(reset_values)
+
+
+out = widgets.interactive_output(func, {'d_cf_z2':d_cf_z2_slider, 'd_cf_x2y2':d_cf_x2y2_slider, 
+                                        'hopping_x':s_h_ab_slider, 'hopping_z':s_h_c_slider,
+                                        'kz':kz_slider, 'Nk_path':Nk_path_slider, 'Nk_FS':Nk_FS_slider});
+out.layout.height = '800px'
+out.layout.width = '800px'
+    
+ui = widgets.HBox([widgets.VBox([d_cf_z2_slider,d_cf_x2y2_slider,s_h_ab_slider,s_h_c_slider]), 
+                         widgets.VBox([kz_slider, Nk_path_slider, Nk_FS_slider]),  widgets.VBox([reset_button])] )
